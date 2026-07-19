9cb586b8b4965075f4f619e510766adf60a76fd8
Anushkaa2306 <bdm.lokendra@gmail.com>
2026-07-20 01:07:57 +0530

    repared the code

--- utils/speech.py
+++ utils/speech.py
@@ -4,14 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
+api_key = os.getenv("GROQ_API_KEY")
 
+if not api_key:
+    raise Exception("GROQ_API_KEY is not set")
+
+client = Groq(api_key=api_key)
 
 def transcribe_audio(audio_path):
     with open(audio_path, "rb") as file:
-
         transcription = client.audio.transcriptions.create(
             file=file,
             model="whisper-large-v3",
